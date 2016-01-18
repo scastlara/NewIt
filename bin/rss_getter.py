29056e6bfc2bbd328ab51d172047519cf3f8b393
Sergio Castillo <s.cast.lara@gmail.com>
2016-01-18 10:12:49 +0100

    Now with functions

--- bin/rss_getter.py
+++ bin/rss_getter.py
@@ -1,15 +1,26 @@
 import feedparser
 import codecs
 
-d = feedparser.parse('http://eldiario.es.feedsportal.com/rss')
-fh = codecs.open("kk.html", "w", "utf-8")
+def write_feed(file, data):
+    fh = codecs.open(file, "w", "utf-8")
 
-fh.write("<html>\n<meta charset=\"UTF-8\"> ")
-for post in d.entries:
+    fh.write("<html>\n<meta charset=\"UTF-8\"> ")
+    fh.write("<h1>" + data.feed.title + "</h1>")
 
-    fh.write("<h1>" + post.title + "</h1>")
-    fh.write ("<p><a href=\"" + post.link + "\">Source </a></p>")
-    fh.write("<p>" + post.published + "</p>")
-    fh.write("<p>" + post.description + "</p>\n")
+    fh.write("<h1> TAGS: \n<p>" + data.feed.tags + "</p></h1>")
+    for post in data.entries:
+        fh.write("<h2>" + post.title + "</h1>")
+        fh.write ("<p><a href=\"" + post.link + "\">Source </a></p>")
+        fh.write("<p>" + post.published + "</p>")
+        fh.write("<p>" + post.description + "</p>\n")
 
-fh.write("</html>")
+        fh.write("</html>")
+
+def read_rss(link):
+    d = feedparser.parse(link)
+    return(d)
+
+
+
+feed = read_rss('http://eldiario.es.feedsportal.com/rss')
+write_feed("kk.html", feed)
