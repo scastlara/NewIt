bc60df0ba211b061d73128cea56bcaaf9b951113
Sergio Castillo <s.cast.lara@gmail.com>
2016-01-20 18:50:41 +0100

    Function to get ID for each article

--- bin/rss_getter.py
+++ bin/rss_getter.py
@@ -20,8 +20,27 @@ def read_rss(link):
     d = feedparser.parse(link)
     return(d)
 
+def get_ID(title, source, date):
+    title = title.encode("ascii", "ignore")
+    words = title.split()
+
+    source_codes = { 'ELDIARIO.ES':001  }
+    title_code = int()
+    max_i = 10
+    i     = 0
+    for word in words:
+        if i < max_i:
+            title_code += ord(word[0])
+            i += 1
+        else:
+            break
+
+    final_code = str()
+    final_code = str(source_codes[source]) + str(title_code) + str(date)
+    print(title_code)
+
 def add_entries(data):
-    db = MySQLdb.connect("localhost","root","awesome24", "news")
+    db     = MySQLdb.connect("localhost","root","5961", "news")
     cursor = db.cursor()
     db.set_character_set('utf8')
     cursor.execute('SET NAMES utf8;')
@@ -34,11 +53,13 @@ def add_entries(data):
     for article in data.entries:
         time.sleep(1)
         identifier = article.title + article.published
-        title = article.title
-        fecha = article.published
-        newsp = "ELDIARIO.ES"
+        title   = article.title
+        fecha   = article.published
+        newsp   = "ELDIARIO.ES"
         content = article.description
 
+        # GET ID FOR DATABASE
+        get_ID(title, newsp, fecha)
         title = title.replace("'", "")
         content=content.replace("'", "")
 
