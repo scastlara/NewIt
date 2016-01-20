dcee8c59beca42ece44ce3eac0119410e39f8284
Adrià Pérez <adriperez24@gmail.com>
2016-01-20 18:54:01 +0100

    Added the date function

--- bin/rss_getter.py
+++ bin/rss_getter.py
@@ -20,11 +20,25 @@ def read_rss(link):
     d = feedparser.parse(link)
     return(d)
 
-def add_entries(data):
-    db = MySQLdb.connect("localhost","root","5961", "news")
-    cursor = db.cursor()
+def date_changer(date):
+	month_dict={ 'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
+	date = date.split(" ")
+	sergio_date = date[4].split(":")[0] + date[4].split(":")[1]
+	new_date = date[3]+"-"+month_dict[date[2]]+"-"+date[1]+" "+date[4]
+	return (new_date)
+	return (sergio_date)
 
+def add_entries(data):
+    db = MySQLdb.connect("localhost","root","awesome24", "news")
+    cursor = db.cursor()
+    db.set_character_set('utf8')
+    cursor.execute('SET NAMES utf8;')
+    cursor.execute('SET CHARACTER SET utf8;')
+    cursor.execute('SET character_set_connection=utf8;')
     fh = codecs.open("test", "w", "utf-8")
+
+    fileout = codecs.open("culo", "w", "utf-8")
+
     for article in data.entries:
         time.sleep(1)
         identifier = article.title + article.published
@@ -33,10 +47,14 @@ def add_entries(data):
         newsp = "ELDIARIO.ES"
         content = article.description
 
-        title.replace("'", r"\'")
-        content.replace("'", r"\'")
-        title = title.encode("ascii", "ignore")
-        content = content.encode("ascii", "ignore")
+
+
+        title = title.replace("'", "")
+        content=content.replace("'", "")
+       
+        print(title)
+        #title = title.encode("ascii", "ignore")
+        #content = content.encode("ascii", "ignore")
 
         fh.write("IDENTIFIER: " + identifier + "\n\n")
         fh.write("TITLE: " + title + "\n\n")
@@ -47,6 +65,7 @@ def add_entries(data):
 
         sql = "INSERT INTO NEWSTABLE(ID, TITLE, FECHA, NEWSPAPER, CONTENT) \
 VALUES ('%s', '%s', '%s', '%s', '%s')" % (title, identifier, fecha, newsp, content)
+        fileout.write(sql)
         try:
             cursor.execute(sql)
             db.commit()
