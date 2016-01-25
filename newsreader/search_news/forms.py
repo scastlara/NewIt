e8d919e07f22a387c410fb80b17b276fd6f1a012
Sergio Castillo <s.cast.lara@gmail.com>
2016-01-25 17:59:50 +0100

    Search by category

--- newsreader/search_news/forms.py
+++ newsreader/search_news/forms.py
@@ -1,8 +1,31 @@
 from django import forms
+import os
 
 '''
 This file has the info needed to create web forms
 '''
+def get_categories():
+    script_path = os.path.realpath(__file__)
+    script_path = script_path.replace("forms.py", "")
+    path = script_path + "static/feeds.tbl"
+    file = open(path, "r")
+    set_of_cat = set()
+    for line in file:
+        line     = line.strip()
+        elements = line.split()
+        set_of_cat.add(elements[1])
+
+    # Add All choice
+    set_of_cat.add("All")
+
+    list_of_cats = list()
+    for element in set_of_cat:
+        view_choice = element.lower()
+        view_choice = view_choice.title()
+        list_of_cats.append((element, view_choice))
+
+    return(sorted(list_of_cats))
 
 class SearchForm(forms.Form):
     sterm = forms.CharField(label='', max_length=100, required=False)
+    categ = forms.ChoiceField(label='', choices=get_categories(), required=False)
--- newsreader/search_news/views.py
+++ newsreader/search_news/views.py
@@ -25,15 +25,26 @@ def index_view(request):
         if form.is_valid():
             # Get what the user wrote:
             search_term = form.cleaned_data['sterm']
+            category    = form.cleaned_data['categ']
             news = list()
+
+
+            sql_query = 'SELECT * FROM search_news_articles'
+
             if len(search_term) > 0:
-                for p in Articles.objects.raw (' SELECT * FROM search_news_articles \
-                                                 WHERE MATCH(title) AGAINST("%s")' % (search_term)):
-                	news.append(p.title) ### AQUI ES ON ESTIC! PRINTA L'ULTIM! CAL FER UN APPEND!
+                sql_query += ' WHERE (MATCH(title) AGAINST("%s") \
+                              OR MATCH(content) AGAINST("%s") )' % (search_term, search_term)
+                if category != "All":
+                    sql_query += ' AND category = "%s"' % (category)
+            elif category != "All":
+                sql_query += ' WHERE category = "%s"' % (category)
+
+            for article in Articles.objects.raw (sql_query):
+            	news.append(article.title)
 
             # Here we would do magic (MySQL search) and we will return something that will go to
             # the template =D
-            return render(request, 'search_news/index.html', {'form': form, 'content': string, 'news': news,	 'term' : search_term} )
+            return render(request, 'search_news/index.html', {'form': form, 'content': sql_query, 'news': news, 'term' : search_term} )
 
     else:
         form = SearchForm()
