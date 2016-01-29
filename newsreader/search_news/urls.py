eabb02fdaa10cf5fef597d443c2fb0584a67761a
Sergio Castillo <s.cast.lara@gmail.com>
2016-01-29 21:43:16 +0100

    Fixed warnings for deprecated syntax in url() function.

--- newsreader/search_news/urls.py
+++ newsreader/search_news/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import include, url
 from . import views
 from django.contrib.auth.views import logout_then_login
+from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^$', views.index_view),
         # Registration URLs
-    url(r'^accounts/register/$', 'search_news.views.register', name='register'),
-    url(r'^accounts/register/complete/$', 'search_news.views.registration_complete',
-     name='registration_complete'),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^accounts/register/$', views.register, name='register'),
+    url(r'^accounts/register/complete/$', views.registration_complete),
+    url(r'^accounts/login/$', login),
     url(r'^accounts/logout/$', lambda request: logout_then_login(request, "/"), name='logout'),
-    url(r'^accounts/loggedin/$', 'search_news.views.loggedin', name='loggedin'),
+    url(r'^accounts/loggedin/$', views.loggedin),
 ]
