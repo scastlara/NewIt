e141dd3c5fe3efca039b1b8aed8e098030f17459
Sergio Castillo <s.cast.lara@gmail.com>
2016-02-02 12:09:13 +0100

    FIXEDDDD

--- newsreader/search_news/admin.py
+++ newsreader/search_news/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Article, Category, Feed, Search_Subscription, Feed_Subscription, Bookmark, Source
+from .models import Article, Category, Feed, Search_Subscription, Source_Subscription, Bookmark, Source
 
 admin.site.register(Article)
 admin.site.register(Category)
 admin.site.register(Feed)
 admin.site.register(Search_Subscription)
-admin.site.register(Feed_Subscription)
+admin.site.register(Source_Subscription)
 admin.site.register(Bookmark)
 admin.site.register(Source)
